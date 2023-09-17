3deda0d675db23cf157aab59cb9db7a5441e2ba6
Ling Wang <2505660478@qq.com>
2023-09-17 21:34:38 +0800

    add medium omit

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,7 @@
-type MyOmit<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+type Exclude<T, U> = T extends U ? never : T;
+
+export type MyOmit<T, K extends keyof T> = MyPick<T, Exclude<keyof T, K>>;
